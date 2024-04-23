dd56915199872016eb4cb34d051f1b8759c837ea
csaenzs <csaenz.0414@gmail.com>
2024-04-23 11:24:38 -0500

    Se modifica reportes Artuculos Vendidos la tabla y el header

--- modules/Report/Resources/views/co-items-sold/report_pdf.blade.php
+++ modules/Report/Resources/views/co-items-sold/report_pdf.blade.php
@@ -38,47 +38,47 @@
                         </thead>
                         <tbody>
                             @php
-                                $total = 0;
-                                $total_tax = 0;
-                                $total_utility = 0;
-                                $total_quantity = 0;
-                                $total_net_value = 0;
-                                $total_cost = 0;
-                                $total_discount = 0;
+                                $totalQuantity = 0;
+                                $totalCost = 0;
+                                $totalNetValue = 0;
+                                $totalUtility = 0;
+                                $totalTax = 0;
+                                $grandTotal = 0;
                             @endphp
+    
                             @foreach($records as $value)
                                 @php
                                     $row = $value->getDataReportSoldItems();
-                                    $total += $row['total'] ?? 0;
-                                    $total_tax += $row['total_tax'] ?? 0;
-                                    $total_utility += $row['utility'] ?? 0;
-                                    $total_quantity += $row['quantity'] ?? 0;
-                                    $total_net_value += $row['net_value'] ?? 0;
-                                    $total_cost += $row['cost'] ?? 0;
-                                    $total_discount += $row['discount'] ?? 0;
+                                    // Sumar los valores a las variables acumuladoras
+                                    $totalQuantity += $row['quantity'];
+                                    $totalCost += $row['cost'];
+                                    $totalNetValue += $row['net_value'];
+                                    $totalUtility += $row['utility'];
+                                    $totalTax += $row['total_tax'];
+                                    $grandTotal += $row['total'];
                                 @endphp
                                 <tr>
                                     <td class="celda">{{ $row['type_name'] }}</td>
                                     <td class="celda">{{ $row['internal_id'] }}</td>
-                                    <td class="celda">{{ preg_replace('/[[:^print:]]/', '', $row['name']) }}</td>
-                                    <td class="celda">{{ $row['quantity'] }}</td>
-                                    <td class="celda">{{ $row['cost'] }}</td>
-                                    <td class="celda">{{ $row['net_value'] }}</td>
-                                    <td class="celda">{{ $row['utility'] }}</td>
-                                    <td class="celda">{{ $row['total_tax'] }}</td>
-                                    <td class="celda">{{ $row['discount'] ?? 0 }}</td>
-                                    <td class="celda">{{ $row['total'] }}</td>
+                                    <td class="celda">{{ $row['name'] }}</td>
+                                    <td class="celda">{{ number_format($row['quantity'], 0, ',', '.') }}</td>
+                                    <td class="celda">{{ number_format($row['cost'], 2, ',', '.') }}</td>
+                                    <td class="celda">{{ number_format($row['net_value'], 2, ',', '.') }}</td>
+                                    <td class="celda">{{ number_format($row['utility'], 2, ',', '.') }}</td>
+                                    <td class="celda">{{ number_format($row['total_tax'], 2, ',', '.') }}</td>
+                                    <td class="celda">{{ number_format($row['total'], 2, ',', '.') }}</td>
                                 </tr>
-                            @endforEach
+                            @endforeach
+    
+                            <!-- Fila de Totales -->
                             <tr>
-                                <td colspan="3" class="celda text-right-td">TOTALES </td>
-                                <td class="celda">{{ $total_quantity }}</td>
-                                <td class="celda">{{ number_format($total_cost, 2) }}</td>
-                                <td class="celda">{{ number_format($total_net_value, 2) }}</td>
-                                <td class="celda">{{ number_format($total_utility, 2) }}</td>
-                                <td class="celda">{{ number_format($total_tax, 2) }}</td>
-                                <td class="celda">{{ number_format($total_discount, 2) }}</td>
-                                <td class="celda">{{ number_format($total, 2) }}</td>
+                                <td class="celda" colspan="3"><strong>Total:</strong></td>
+                                <td class="celda"><strong>{{ number_format($totalQuantity, 0, ',', '.') }}</strong></td>
+                                <td class="celda"><strong>{{ number_format($totalCost, 2, ',', '.') }}</strong></td>
+                                <td class="celda"><strong>{{ number_format($totalNetValue, 2, ',', '.') }}</strong></td>
+                                <td class="celda"><strong>{{ number_format($totalUtility, 2, ',', '.') }}</strong></td>
+                                <td class="celda"><strong>{{ number_format($totalTax, 2, ',', '.') }}</strong></td>
+                                <td class="celda"><strong>{{ number_format($grandTotal, 2, ',', '.') }}</strong></td>
                             </tr>
                         </tbody>
                     </table>
