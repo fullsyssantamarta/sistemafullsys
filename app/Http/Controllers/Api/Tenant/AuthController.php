378891e2aa7fb5b58e484a46b0fdc16a483eeaee
rash07 <sh.raul.jgb@gmail.com>
2024-08-02 17:33:11 -0500

    feature | autologin mediante api_token

--- app/Http/Controllers/Api/Tenant/AuthController.php
+++ app/Http/Controllers/Api/Tenant/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api\Tenant;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use App\Models\Tenant\User;
 
 class AuthController extends Controller
 {
@@ -35,5 +37,20 @@ class AuthController extends Controller
         ];
 
         return response()->json(['data' => $userResponse, 'code' => Response::HTTP_OK], Response::HTTP_OK);
+    }
+
+    public function loginToken($token)
+    {
+        $user = User::where('api_token', $token)->first();
+        if($user) {
+            Auth::loginUsingId($user->id);
+            if (Auth::guard('web')->check()) {
+                return redirect('/dashboard');
+            } else {
+                return response()->json(['error' => 'Credenciales invalidas', 'code' => Response::HTTP_UNAUTHORIZED], Response::HTTP_UNAUTHORIZED);
+            }
+        } else {
+            return response()->json(['error' => 'Credenciales invalidas', 'code' => Response::HTTP_UNAUTHORIZED], Response::HTTP_UNAUTHORIZED);
+        }
     }
 }
