4eaf1b6c73cad58025a7d0605a5c3567bb7405c7
Rash07 <sh.raul.jgb@gmail.com>
2025-02-17 11:39:45 -0500

    fix | ajustes minimos sobre .env y registro local en apidian

--- modules/Factcolombia1/Traits/System/CompanyTrait.php
+++ modules/Factcolombia1/Traits/System/CompanyTrait.php
@@ -88,8 +88,6 @@ trait CompanyTrait
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS,($data_companiee));
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json',
             'Accept: application/json',
